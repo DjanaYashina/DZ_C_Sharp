affc9093837994d2e6a01c25445db6fbb217e1a6
Djana Yashina <work1099@mail.ru>
2023-06-10 12:21:05 +0300

    добавлено решение ДЗ задача 6

---
+++ Lesson1/DZunit6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число
+// чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число:");
+string? numberOne = Console.ReadLine();
+int nOne = Convert.ToInt32(numberOne); 
+
+if (nOne % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}

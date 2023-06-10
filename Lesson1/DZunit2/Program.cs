f7c341f7a3e19d60f2227be22e74cb523ed675ed
Djana Yashina <work1099@mail.ru>
2023-06-10 11:19:18 +0300

    добавлено решение ДЗ задача 2

---
+++ Lesson1/DZunit2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 2: Напишите программу, которая на вход 
+// принимает два числа и выдаёт, какое число
+// большее, а какое меньшее.
+Console.WriteLine("Введите число 1:");
+string? numberOne = Console.ReadLine();
+int nOne = Convert.ToInt32(numberOne); 
+
+Console.WriteLine("Введите число 2:");
+string? numberTwo = Console.ReadLine();
+int nTwo = Convert.ToInt32(numberTwo);
+int Max = 0;
+int Min = 0;
+
+if (nOne > nTwo)
+{
+    Max = nOne;
+    Console.Write("Max = ");
+    Console.WriteLine(Max);
+    Min = nTwo;
+    Console.Write("Min = ");
+    Console.Write(Min);
+}
+else
+{
+    Max = nTwo;
+    Console.Write("Max = ");
+    Console.WriteLine(Max);
+    Min = nOne;
+    Console.Write("Min = ");
+    Console.Write(Min);
+}

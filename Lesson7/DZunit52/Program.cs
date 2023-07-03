2aeca587056ea648c2b2482c46e38843ef4287fb
Djana Yashina <work1099@mail.ru>
2023-07-03 12:53:57 +0300

    добавлено дз по 7 семинару

--- Lesson7/DZunit52/Program.cs
+++ Lesson7/DZunit52/Program.cs
@@ -5,75 +5,27 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-// int[,] GetRandomMatrix(int rows, int columns)
-// {
-//     int[,] matrix = new int[rows, columns];
-//     for (int i=0; i< matrix.GetLength(0); i++)
-//     {
-//       for (int j=0; j < matrix.GetLength(1); j++)
-//       {
-//         matrix[i,j] = Random.Shared.Next(1, 10);
-//       }  
-//     }
-//     return matrix;
-// } 
-
-// void PrintMatrix(int[,] matrix)
-// {
-//    for (int i=0; i< matrix.GetLength(0); i++)
-//     {
-//       for (int j=0; j < matrix.GetLength(1); j++)
-//       {
-//         Console.Write($"{matrix[i, j]} ");
-//       } 
-//       Console.WriteLine(); 
-//     } 
-
-// }
-
-
-// double MiddleSum(int[,] matrix)
-// {
-// double middle = 0;
-// int sum = 0;
-// for (int i=0; i< matrix.GetLength(0); i++)
-//     {
-//     for (int j=0; j < matrix.GetLength(1); j++)
-//       {
-//         sum = ;
-//         middle = sum/matrix.GetLength(1);
-//       }
-      
-//     }
-// return middle; 
-// }
-// const int ROWS = 3;
-// const int COLUMNS = 4;
-// int[,] myMatrix = GetRandomMatrix(ROWS, COLUMNS);
-// PrintMatrix(myMatrix);
-
-// double MidSum = MiddleSum(myMatrix);
-// Console.WriteLine(string.Join(", ",MidSum));
-
-Random random = new Random();
-int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
-for (int i = 0; i < arr.GetLength(0); i++)
+const int rows = 3;
+const int columns = 4;
+int[,] matrix = new int[rows, columns];
+for (int i=0; i< matrix.GetLength(0); i++)
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    for (int j=0; j < matrix.GetLength(1); j++)
     {
-        arr[i, j] = random.Next(1, 10);
-        Console.Write(arr[i, j] + " ");
+     matrix[i,j] = Random.Shared.Next(1, 10);
+     Console.Write($"{matrix[i, j]} ");
     }
-    Console.WriteLine();
+    Console.WriteLine();  
 }
-Console.WriteLine("---------------------------");
-// Console.WriteLine(arr.GetLength(0));
-for (int j = 0; j < arr.GetLength(1); j++)
+    
+
+for (int j=0; j < matrix.GetLength(1); j++)
 {
     double sum = 0;
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        sum += arr[i, j];
-    }
-    Console.Write($"{ sum / arr.GetLength(0):f2} ");
+    for (int i=0; i< matrix.GetLength(0); i++)
+      {
+        sum = matrix[i,j] + sum;
+       
+      }
+     Console.Write($"{ sum / matrix.GetLength(0):f1} "); 
 }
